ad1a4f0847910887feac62fecb414235ce8cd0e8
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2021-08-20 03:42:14 +0100

    chore: add jest scripts

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from '../models'
+import { AccountModel } from '@/domain/models'
 
 export interface Authentication {
   auth(params: Authentication.Params): Promise<Authentication.Result>
