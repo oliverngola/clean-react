6e3b2186241aeb189f7c60289dda7793461d6b7d
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2021-08-22 11:00:48 +0100

    test: ensure AxiosHttpClient calls axios with correct verb

--- tests/infra/http/axios-http-client.spec.ts
+++ tests/infra/http/axios-http-client.spec.ts
@@ -12,10 +12,10 @@ const makeSut = (): {sut: AxiosHttpClient} => {
 }
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL', async () => {
+  test('Should call axios with correct URL and verb', async () => {
     const { sut } = makeSut()
     const url = faker.internet.url()
     await sut.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
