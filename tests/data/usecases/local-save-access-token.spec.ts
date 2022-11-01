b904be70fca50c385104086a90f52b455509216f
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2022-11-01 23:40:44 +0100

    test: ensure LocalSaveAccessToken calls SetStorage with correct value

---
+++ tests/data/usecases/local-save-access-token.spec.ts
@@ -0,0 +1,14 @@
+import { LocalSaveAccessToken } from '@/data/usecases'
+import { SetStorageSpy } from '@/tests/data/mocks'
+import faker from 'faker'
+
+describe('LocalSaveAcessToken', () => {
+  test('Should call SetStorage with correct value', async () => {
+    const setStorageSpy = new SetStorageSpy()
+    const sut = new LocalSaveAccessToken(setStorageSpy)
+    const accessToken = faker.datatype.uuid()
+    await sut.save(accessToken)
+    expect(setStorageSpy.key).toBe('accessToken')
+    expect(setStorageSpy.value).toBe(accessToken)
+  })
+})
