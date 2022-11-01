2e8c13781c50accd3e19660fd5c6abe5a3787120
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2022-11-01 23:08:08 +0100

    feat: add SaveAccessToken usecase

--- src/domain/usecases/save-access-token.ts
+++ src/domain/usecases/save-access-token.ts
@@ -1,9 +1,3 @@
 export interface SaveAcessToken {
-  save: (params: SaveAcessToken.Params) => Promise<void>
-}
-
-export namespace SaveAcessToken {
-  export type Params = {
-    accessToken: string
-  }
+  save: (accessToken: string) => Promise<void>
 }
