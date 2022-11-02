1dcdd3d8f834cf8d308804928d03b46f5343d0df
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2022-11-02 20:38:48 +0100

    test: ensure RemoteAddAccount calls HttpPostClient with correct URL and body

---
+++ tests/data/usecases/remote-add-account.spec.ts
@@ -0,0 +1,36 @@
+import { HttpPostClientSpy } from '@/tests/data/mocks'
+import { mockAddAccount } from '@/tests/domain/mocks'
+import { RemoteAddAccount } from '@/data/usecases'
+import { AddAccount } from '@/domain/usecases'
+
+import faker from 'faker'
+
+type SutTypes = {
+  sut: RemoteAddAccount
+  httpPostClientSpy: HttpPostClientSpy<AddAccount.Params, AddAccount.Result>
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy<AddAccount.Params, AddAccount.Result>()
+  const sut = new RemoteAddAccount(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
+describe('RemoteAddAccount', () => {
+  test('Should call HttpPostClient with correct URL', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.add(mockAddAccount())
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('Should call HttpPostClient with correct body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const addAccountParams = mockAddAccount()
+    await sut.add(addAccountParams)
+    expect(httpPostClientSpy.body).toEqual(addAccountParams)
+  })
+})
--- tests/domain/mocks/mock-account.ts
+++ tests/domain/mocks/mock-account.ts
@@ -1,4 +1,4 @@
-import { Authentication } from '@/domain/usecases'
+import { AddAccount, Authentication } from '@/domain/usecases'
 
 import faker from 'faker'
 
@@ -6,6 +6,16 @@ export const mockAuthentication = (): Authentication.Params => ({
   email: faker.internet.email(),
   password: faker.internet.password()
 })
+
+export const mockAddAccount = (): AddAccount.Params => {
+  const password = faker.internet.password()
+  return {
+    name: faker.random.word(),
+    email: faker.internet.email(),
+    password: password,
+    passwordConfirmation: password
+  }
+}
 
 export const mockAccountModel = (): Authentication.Result => ({
   name: faker.name.findName(),
