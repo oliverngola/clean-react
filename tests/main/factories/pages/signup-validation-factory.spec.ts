3ac0a1ffaf94e3555d861ee866f3377e08709b15
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2022-11-18 22:16:01 +0100

    test: add SignUp composition

---
+++ tests/main/factories/pages/signup-validation-factory.spec.ts
@@ -0,0 +1,14 @@
+import { makeSignUpValidation } from '@/main/factories/pages'
+import { ValidationBuilder, ValidationComposite } from '@/validation/validators'
+
+describe('SignUpValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(new ValidationComposite([
+      ...ValidationBuilder.field('name').required().min(5).build(),
+      ...ValidationBuilder.field('email').required().email().build(),
+      ...ValidationBuilder.field('password').required().min(5).build(),
+      ...ValidationBuilder.field('passwordConfirmation').required().min(5).build()
+    ]))
+  })
+})
