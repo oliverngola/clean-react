0c291ded4df545a5e915948f8dc96e8d2f58501a
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2022-12-05 10:19:26 +0100

    test: prevent Login to call submit multiple times

--- tests/e2e/cypress/integration/login.spec.ts
+++ tests/e2e/cypress/integration/login.spec.ts
@@ -108,4 +108,17 @@ describe('Login', () => {
       return assert.isOk(window.localStorage.getItem('accessToken'))
     })
   })
+
+  it('Should prevent multiple submits', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.random.words()
+      }
+    }).as('request')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').dblclick()
+    cy.get('@request.all').should('have.length', 1)
+  })
 })
