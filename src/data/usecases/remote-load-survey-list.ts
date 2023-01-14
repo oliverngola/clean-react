099b9dbb82db7e3bd7bb7e33745d515c93c72818
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-01-14 22:09:26 +0100

    feat: ensure RemoteLoadSurveyList returns a list on 200

--- src/data/usecases/remote-load-survey-list.ts
+++ src/data/usecases/remote-load-survey-list.ts
@@ -1,11 +1,11 @@
 import { LoadSurveyList } from '@/domain/usecases'
-import { HttpGetClient, HttpStatusCode } from '@/data//protocols'
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols'
 import { UnexpectedError } from '@/domain/errors'
 
 export class RemoteLoadSurveyList implements LoadSurveyList {
   constructor (
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<LoadSurveyList.Result>
   ) {}
 
   async loadAll (): Promise<LoadSurveyList.Result> {
@@ -13,7 +13,7 @@ export class RemoteLoadSurveyList implements LoadSurveyList {
       url: this.url
     })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return null
+      case HttpStatusCode.ok: return httpResponse.body
       default: throw new UnexpectedError()
     }
   }
