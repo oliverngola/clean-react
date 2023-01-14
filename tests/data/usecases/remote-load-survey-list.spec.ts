ae15565638fb28e535f0a8bd6c038faae05fd153
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-01-14 21:32:10 +0100

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 400

--- tests/data/usecases/remote-load-survey-list.spec.ts
+++ tests/data/usecases/remote-load-survey-list.spec.ts
@@ -35,4 +35,13 @@ describe('RemoteLoadSurveyList', () => {
     const promise = sut.loadAll()
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpGetClient returns 400', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const promise = sut.loadAll()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
