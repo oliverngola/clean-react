6de9b63d5243f79593ab8ce426101c4aa9dc2e50
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-04-08 19:49:07 +0100

    test: LocalStorageAdapter calls removeItem if value is null

--- tests/infra/cache/local-storage-adapter.spec.ts
+++ tests/infra/cache/local-storage-adapter.spec.ts
@@ -1,4 +1,3 @@
-import { AccountModel } from '@/domain/models'
 import { LocalStorageAdapter } from '@/infra/cache'
 import faker from 'faker'
 import 'jest-localstorage-mock'
@@ -13,15 +12,22 @@ describe('LocalStorageAdapter', () => {
   test('Should call localStorage.setItem with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
-    const value = faker.random.objectElement<AccountModel>()
+    const value = faker.random.objectElement<any>()
     sut.set(key, value)
     expect(localStorage.setItem).toBeCalledWith(key,JSON.stringify(value))
+  })
+
+  test('Should call localStorage.removeItem if value is null', () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    sut.set(key, undefined)
+    expect(localStorage.removeItem).toBeCalledWith(key)
   })
 
   test('Should call localStorage.getItem with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
-    const value = faker.random.objectElement<AccountModel>()
+    const value = faker.random.objectElement<any>()
     const getItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
     const obj = sut.get(key)
     expect(obj).toEqual(value)
