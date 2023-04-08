96094f1a1bf1914a393e412d91b5beea17b4f9c9
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-04-08 22:55:32 +0100

    refactor: change SurveyList to use costum hook

---
+++ src/presentation/hooks/use-error-handler.ts
@@ -0,0 +1,20 @@
+import { AccessDeniedError } from '@/domain/errors'
+import { ApiContext } from '@/presentation/contexts'
+import { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+
+type CallbackType = (error: Error) => void
+type ResultType = CallbackType
+
+export const useErrorHandler = (callback: CallbackType): ResultType => {
+  const history = useHistory()
+  const { setCurrentAccount } = useContext(ApiContext)
+  return (error: Error): void => {
+    if (error instanceof AccessDeniedError) {
+      setCurrentAccount(undefined)
+      history.replace('/login')
+    } else {
+      callback(error)
+    }
+  }
+}
