70093bfdd00bb32f448b67659ba96a3fe06ba616
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-04-08 19:46:25 +0100

    refactor: remove unused cod

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,11 +1,8 @@
-import { UnexpectedError } from '@/domain/errors'
+
 import { AccountModel } from '@/domain/models'
 import { makeLocalStorageAdapter } from '@/main/factories/cache'
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-  if (!account?.accessToken) {
-    throw new UnexpectedError()
-  }
   makeLocalStorageAdapter().set('account', account)
 }
 
