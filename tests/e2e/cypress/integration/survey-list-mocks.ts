621a00bdc54b98e4bd431060fe67c1195ecf46bb
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-05-25 10:59:29 +0100

    test: ensure SurveyList logout on AccessDeniedError

--- tests/e2e/cypress/integration/survey-list-mocks.ts
+++ tests/e2e/cypress/integration/survey-list-mocks.ts
@@ -1,3 +1,4 @@
 import * as http from '../support/http-mocks'
 
 export const mockUnexpectedError = (): void => { http.mockServerError(/surveys/, 'GET') }
+export const mockAcessDeniedError = (): void => { http.mockForbbidenError(/surveys/, 'GET') }
