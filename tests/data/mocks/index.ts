795c2fb98327a2b32c9b72ca2898be45607d0762
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-06-08 00:46:22 +0100

    test: ensure RemoteLoadSurveyResutl returns a SurveyResultModel on 200

--- tests/data/mocks/index.ts
+++ tests/data/mocks/index.ts
@@ -1,3 +1,4 @@
 export * from './mock-http'
 export * from './mock-storage'
-export * from './mock-remote-survey'
+export * from './mock-remote-survey-list'
+export * from './mock-remote-survey-result'
