d64c5b5a3cb2d1a9b0ae80e4532f54f5c0a27eba
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-06-07 22:34:32 +0100

    chore: config travis to use cypress

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:8080',
     fixturesFolder: 'tests/e2e/cypress/fixtures',
     supportFile: 'tests/e2e/cypress/support/e2e.ts',
-    specPattern: 'tests/e2e/cypress/integration/**/*.spec.ts'
+    specPattern: 'tests/e2e/cypress/integration/**/*.spec.ts',
+    video: false
   }
 })
