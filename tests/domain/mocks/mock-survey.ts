f949de900eecaa16544e197dc5872d3f69367832
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-06-16 09:12:24 +0100

    test: ensure SurveyResult presents correct result on success

--- tests/domain/mocks/mock-survey.ts
+++ tests/domain/mocks/mock-survey.ts
@@ -22,11 +22,11 @@ export const mockSurveyResultModel = (): LoadSurveyResult.Model => ({
     answer: faker.random.word(),
     count: faker.datatype.number(),
     percent: faker.datatype.number(100),
-    isCurrentAccountAnswer: faker.datatype.boolean()
+    isCurrentAccountAnswer: true
   },{
     answer: faker.random.word(),
     count: faker.datatype.number(),
     percent: faker.datatype.number(100),
-    isCurrentAccountAnswer: faker.datatype.boolean()
+    isCurrentAccountAnswer: false
   }]
 })
